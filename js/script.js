02ff8334f92d316ac2a962708dbe3a36786831aa
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2026-03-16 17:30:33 -0300

    adições de interatividade com JS. Interatividade em orcamento.html e nos links da header

--- js/script.js
+++ js/script.js
@@ -1,35 +1,60 @@
-const params = new URLSearchParams(location.search);
+//ativar links do menu
+const links = document.querySelectorAll('.header-menu a');
+console.log(links)
+function ativarLink(link){
+  const url = location.href
+  const href= link.href
 
-params.forEach((item) => {
-  const element = document.querySelector(`[type="radio"][value="${item}"]`);
-  if (element) element.checked = true;
-});
-
-const dts = document.querySelectorAll("dt");
-
-dts.forEach((dt) => {
-  dt.addEventListener("click", () => {
-    dt.parentElement.classList.toggle("ativa");
-  });
-});
-
-const links = document.querySelectorAll(".header-menu a");
-
-links.forEach((link) => {
-  if (location.href.includes(link.href)) {
-    link.classList.add("ativo");
+  if(url.includes(href)){
+    link.classList.add('ativo')
   }
-});
+}
+links.forEach(ativarLink)
 
-const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector(".bicicleta-imagens");
+//ativar items do orçamento
+const parametros= new URLSearchParams(location.search) // retorna uma array com os parametros de busca
 
-galeria.forEach((img) => {
-  img.addEventListener("click", () => {
-    if (matchMedia("(min-width: 1000px)").matches) {
-      galeriaContainer.prepend(img);
-    }
-  });
-});
-
-// if (window.SimpleAnime) new SimpleAnime();
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro)
+  if(elemento){
+  elemento.checked = true
+  }
+  console.log(elemento)
+}
+parametros.forEach(ativarProduto)
+console.log(parametros)
+// const params = new URLSearchParams(location.search);
+//
+// params.forEach((item) => {
+//   const element = document.querySelector(`[type="radio"][value="${item}"]`);
+//   if (element) element.checked = true;
+// });
+//
+// const dts = document.querySelectorAll("dt");
+//
+// dts.forEach((dt) => {
+//   dt.addEventListener("click", () => {
+//     dt.parentElement.classList.toggle("ativa");
+//   });
+// });
+//
+// const links = document.querySelectorAll(".header-menu a");
+//
+// links.forEach((link) => {
+//   if (location.href.includes(link.href)) {
+//     link.classList.add("ativo");
+//   }
+// });
+//
+// const galeria = document.querySelectorAll(".bicicleta-imagens img");
+// const galeriaContainer = document.querySelector(".bicicleta-imagens");
+//
+// galeria.forEach((img) => {
+//   img.addEventListener("click", () => {
+//     if (matchMedia("(min-width: 1000px)").matches) {
+//       galeriaContainer.prepend(img);
+//     }
+//   });
+// });
+//
+// // if (window.SimpleAnime) new SimpleAnime();
